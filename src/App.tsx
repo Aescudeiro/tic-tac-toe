31ffe27bca35fb6e6079aa7f9aaa38ffddb9f213
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2022-03-13 16:09:15 -0100

    chore: tailwind and postcss init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import "./App.css";
+
 function App() {
-    return <div></div>;
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-black">
+            <h1 className="text-white">Hello Tic-Tac-Toe</h1>
+        </div>
+    );
 }
 
 export default App;
