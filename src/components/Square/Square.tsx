6a6a06f74592a347e6a087130f0e3c556e5c6800
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2022-03-14 12:35:56 -0100

    style: addde hover bg color to the squares

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -11,7 +11,7 @@ export const Square: FC<Props> = (props) => {
 
     return (
         <button
-            className="w-24 h-24 border-2 text-3xl rounded border-black border-solid"
+            className="w-24 h-24 border-2 text-3xl rounded border-black border-solid hover:bg-slate-400"
             onClick={onClick}
         >
             {player}
